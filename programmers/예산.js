e6c6d98013a707eb27ae2a6f0281f4358ae91667
seohyunsim <ssh4563@naver.com>
2021-12-31 15:49:44 +0900

    예산

---
+++ programmers/예산.js
@@ -0,0 +1,11 @@
+function solution(d, budget) {
+  d.sort((a, b) => a - b);
+  let answer = 0;
+  for (let i = 0; i < d.length; i++) {
+    if (d[i] <= budget) {
+      answer++;
+      budget -= d[i];
+    }
+  }
+  return answer;
+}
