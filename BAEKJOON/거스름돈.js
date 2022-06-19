788ff78fa1c54e8c6188cff65d10ffc5c078f5f1
seohyunsim <ssh4563@naver.com>
2022-06-19 17:15:58 +0900

    거스름돈

--- BAEKJOON/거스름돈.js
+++ BAEKJOON/거스름돈.js
@@ -5,9 +5,9 @@ let count = 0;
 let answer = 0;
 
 for (let i = 0; i < coin.length; i++) {
-    count = Math.floor(result / coin[i]);
-    result -= count * coin[i]; 
-    answer += count;
+  count = Math.floor(result / coin[i]);
+  result -= count * coin[i];
+  answer += count;
 }
 
 console.log(answer);
