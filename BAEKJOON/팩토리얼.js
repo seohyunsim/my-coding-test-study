c943c282d6a492424f6c735b0cf8967cf156062a
seohyunsim <ssh4563@naver.com>
2022-06-28 23:18:03 +0900

    팩토리얼

--- BAEKJOON/팩토리얼.js
+++ BAEKJOON/팩토리얼.js
@@ -1,8 +1,8 @@
 const n = parseInt(require("fs").readFileSync("/dev/stdin").toString());
 
-function solution(n) {
+const solution = (n) => {
   n === 0 ? (n = 1) : (n *= solution(n - 1));
   return n;
-}
+};
 
 console.log(solution(n));
