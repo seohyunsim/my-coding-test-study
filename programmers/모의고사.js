1470fbeae5a84e64bedb3dfc4335f7c471456a94
seohyunsim <ssh4563@naver.com>
2022-06-30 22:51:15 +0900

    모의고사

--- programmers/모의고사.js
+++ programmers/모의고사.js
@@ -1,4 +1,4 @@
-function solution(answers) {
+const solution = (answers) => {
   const p1 = [1, 2, 3, 4, 5];
   const p2 = [2, 1, 2, 3, 2, 4, 2, 5];
   const p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
@@ -15,4 +15,4 @@ function solution(answers) {
   if (s3 === max) answer.push(3);
 
   return answer;
-}
+};
