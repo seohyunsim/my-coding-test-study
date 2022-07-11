8698d4d4522b66ed243c5c48d6e281a10a992318
seohyunsim <ssh4563@naver.com>
2022-07-11 23:28:53 +0900

    실패율

--- programmers/실패율.js
+++ programmers/실패율.js
@@ -1,16 +1,15 @@
-function solution(N, stages) {
-    const answer = [];
-    let players = stages.length;
+const solution = (N, stages) => {
+  const answer = [];
+  let players = stages.length;
 
-    for (let i = 1; i < N + 1; i++) {
-        let result = stages.filter(index => index === i).length;
-        
-        if (result === 0) {
-            answer.push({index: i, rate: 0});
-        }
-        else answer.push({index: i, rate: result/players});
-        players -= result;
-    }
-    answer.sort((a, b) => b.rate - a.rate);
-    return answer.map(ele => ele.index);
-}
+  for (let i = 1; i < N + 1; i++) {
+    let result = stages.filter((index) => index === i).length;
+
+    if (result === 0) {
+      answer.push({ index: i, rate: 0 });
+    } else answer.push({ index: i, rate: result / players });
+    players -= result;
+  }
+  answer.sort((a, b) => b.rate - a.rate);
+  return answer.map((ele) => ele.index);
+};
