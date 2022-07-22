bc3f22ddc49ab52d6ba795b34fbcaf7506a4b06d
seohyunsim <ssh4563@naver.com>
2022-07-22 23:18:20 +0900

    음양더하기

--- programmers/음양-더하기.js
+++ programmers/음양-더하기.js
@@ -1,6 +1,6 @@
-function solution(absolutes, signs) {
+const solution = (absolutes, signs) => {
   for (let i = 0; i < absolutes.length; i++) {
     signs[i] ? absolutes[i] : (absolutes[i] = -absolutes[i]);
   }
   return absolutes.reduce((a, b) => a + b);
-}
+};
