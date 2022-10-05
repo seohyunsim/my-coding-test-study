63d60cf4b747199e5f1ffa70117f16c591ecbb7f
seohyunsim <ssh4563@naver.com>
2022-10-05 23:45:04 +0900

    폰켓몬

--- programmers/폰켓몬.js
+++ programmers/폰켓몬.js
@@ -1,8 +1,8 @@
-const solution = (nums) => {
+function solution(nums) {
   const newArr = nums.filter((item, idx) => nums.indexOf(item) === idx);
   const halfNums = nums.length / 2;
   const phoneNums = newArr.length;
 
   if (halfNums < phoneNums) return halfNums;
   else return phoneNums;
-};
+}
